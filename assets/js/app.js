7b360c9d0f1cd5a5ccc42dc44f37bc638af0b4da
Alex Osterholt <aosterholt@comcast.net>
2019-02-14 19:16:27 -0800

    Initial push

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ var chartGroup = svg.append("g")
     .attr("transform",`translate(${margin.left},${margin.top})`);
 
 d3.csv("assets/data/data.csv")
-.then(function( popstats){
+.then(function(popstats){
     // if(error) throw error;
     popstats.forEach(function(record){
         record.smokes = +record.smokes;
